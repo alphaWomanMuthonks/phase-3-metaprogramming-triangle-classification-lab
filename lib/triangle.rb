f94c5b232e38d819e1ac56173b39988f5b679bff
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-10-27 09:05:53 +0300

    classifying types of triangles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  def initialize(length, width, height)
+    @sides = [length, width, height]
 end
+def kind
+  if valid_triangle?
+    if @sides.uniq.count == 1
+      :equilateral
+      elsif @sides.uniq.count == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "you must give valid sides of a triangle!"
+    end
+  end
+
+  private
+  def valid_triangle?
+    @sides.each {|side| side > 0} && @sides.sort[0] + @sides.sort[1] > @sides.sort[2]
+  end
+end
+
